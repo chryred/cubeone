771ba6a2a1dc7aa333720dd460a29b5057233c27
173917 <173917@IC0-W-N08359R.ssg.global>
2019-11-19 10:01:25 +0900

    메소드 추가

--- src/main/java/com/shinsegae/smon/model/CubeoneVO.java
+++ src/main/java/com/shinsegae/smon/model/CubeoneVO.java
@@ -16,4 +16,16 @@ public class CubeoneVO {
 	private String columnName;
 	private String errorMsg;
 	private String retMsg;
+	
+	public String toParamValue() {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("msg=").append(msg)
+		  .append("&").append("crudLog=").append(crudLog)
+		  .append("&").append("itemCd=").append(itemCd)
+		  .append("&").append("tableName=").append(tableName)
+		  .append("&").append("columnName=").append(columnName);
+		
+		return sb.toString();
+	}
 }
